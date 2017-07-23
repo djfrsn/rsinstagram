4089b8962a4ba9a483ab3e74acd7a6295e5752ee
Dennis Jefferson <dennismjefferson@gmail.com>
2017-07-22 22:13:11 -0700

    Comment page style progress

--- src/components/comments/comments.body.js
+++ src/components/comments/comments.body.js
@@ -1,16 +1,36 @@
 import Body from 'components/app/body';
 import Icon from 'components/partials/faIcon';
+import UserThumbnail from 'components/partials/userThumbnail';
+import 'components/comments/comments.body.scss';
 
 const CommentsBody = props =>
   <Body>
-    <div className="comment">
-      <image src="userprofile" className="userprofile" />
-      <p className="username">nerdexplainsitall</p>
-      <p className="msg">nerdexplainsitall</p>
-      <Icon name="heart" />
-      <p className="timestamp">4d</p>
-      <p className="likecount">1 like</p>
-      <p className="reply">reply</p>
+    <div className="comment-wrapper">
+      <div className="comment-row">
+        <div className="comment-userthumbnail-wrapper">
+          <UserThumbnail className="comment-userthumbnail" />
+        </div>
+        <div className="comment-msg-wrapper">
+          <p className="msg">
+            <span className="comment-msg-username">nerdexplainsitall</span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            lobortis sollicitudin est ac auctor. Fusce tellus mauris, pharetra a
+            quam eu, sollicitudin ornare mi. Vestibulum eu luctus leo. Aenean
+            eget aliquam risus.
+          </p>
+        </div>
+        <div className="comment-like-wrapper">
+          <Icon name="heart-o" />
+        </div>
+      </div>
+      <div className="comment-footer">
+        <p>
+          <span className="comment-footer-timestamp">4d</span>{' '}
+          <span className="comment-footer-likecount">1 like</span>{' '}
+          <span className="comment-footer-reply">reply</span>
+        </p>
+      </div>
+      <span className="comment-linebreak" />
     </div>
   </Body>;
 
--- src/components/comments/comments.header.js
+++ src/components/comments/comments.header.js
@@ -1,10 +1,18 @@
+import { Link } from 'react-router';
 import Header from 'components/app/header';
 import Icon from 'components/partials/faIcon';
+import 'components/comments/comments.header.scss';
 
 const CommentsHeader = props =>
-  <Header>
-    <Icon name="leftarrow" />
-    <h1 className="commentstitle">Comments</h1>
+  <Header className="commentsheader">
+    <div className="commentsheader-backicon-wrapper">
+      <Link to="/">
+        <Icon name="angle-left" size="3x" className="commentsheader-backicon" />
+      </Link>
+    </div>
+    <div className="commentsheader-title-wrapper">
+      <h1 className="commentsheader-title">Comments</h1>
+    </div>
   </Header>;
 
 export default CommentsHeader;
